b62e690ded2734e24029080bc4e637af979eb4ba
Macuilxochitl <admin@furrylop.com>
2017-07-14 00:53:18 +0800

    更新了一下页面.

--- bl/models.py
+++ bl/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class company(models.Model):
+	id=models.AutoField("ID",primary_key=True)
+	name=models.CharField(max_length=60)
+	note=models.TextField()
+	createTime=models.DateTimeField(auto_now_add=True)
+	lastSaveTime=models.DateTimeField(auto_now=True)
+	isTrain=models.BooleanField(default=False)
+
+	def __str__(self):
+		return "id="+str(self.id)+",name="+self.name+",isTrain="+self.isTrain
